3db26385a76404ed94e5195285f87562cab3b1a6
user <joydeep.adhikary@yahoo.co.in>
2021-07-27 17:16:53 +0530

    changes

--- src/javacore8/GreetingMain.java
+++ src/javacore8/GreetingMain.java
@@ -1,10 +1,15 @@
 package javacore8;
 
 public class GreetingMain {
+	
+	public void takeGreetingI(GreetingI greetingI) {
+		greetingI.function();
+	}
 
 	public static void main(String[] args) {
-		GreetingI greeting1=new GreetingImpl();
-		GreetingI greeting2=()->System.out.println("Lambda Implementation of Interface");
+		GreetingMain greetingMain = new GreetingMain();
+		GreetingI greeting1=new GreetingImpl();//Here Interface Instance is a refrence to a Object of Implementing Class.
+		
 		GreetingI greetinganonymous = new GreetingI() {
 			
 			@Override
@@ -12,7 +17,12 @@ public class GreetingMain {
 				System.out.println("anonymous implementation of interface");
 				
 			}
-		}; 
+		}; //Here Interface Instance is reference to a anonymous Object
+		
+		
+		GreetingI greeting2=()->System.out.println("Lambda Implementation of Interface"); //But here, Interface is reference to a functionality only as functional interface instance.
+		
+		greetingMain.takeGreetingI(()->System.out.println("Passing Lamda as Argument to method"));
 		
 		greeting1.function();
 		greeting2.function();
@@ -20,4 +30,5 @@ public class GreetingMain {
 
 	}
 
+	
 }
